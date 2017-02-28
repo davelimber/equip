9b7cd8259bab7f8eb40800cb25d14690058989ce
Jamie Lacivita <JCLacivita@gmail.com>
2017-02-28 07:07:20 -0700

    added form to enter info

--- src/components/EnterInfo.vue
+++ src/components/EnterInfo.vue
@@ -2,50 +2,56 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
 
+    <div class="container">
+
     <form class="col s12">
       <div class="row">
-        <div class="input-field col s6">
-          <input id="icon_prefix" type="text" class="validate">
-          <label for="icon_prefix">Company name</label>
+        <div class="input-field col s6 offset-s3 offset-s3">
+          <input type="text" name="CompanyName" placeholder="Company Name" class="validate">
+          <!--<label for="icon_prefix">Company name</label>-->
         </div>
       </div>
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s6 offset-s3">
           <input id="icon_telephone" type="tel" class="validate">
           <label for="icon_telephone">Telephone</label>
         </div>
       </div>
 
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s6 offset-s3">
           <input id="icon_prefix" type="text" class="validate">
           <label for="icon_prefix">Street Address</label>
         </div>
       </div>
 
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s6 offset-s3">
           <input id="icon_prefix" type="text" class="validate">
           <label for="icon_prefix">City</label>
         </div>
       </div>
 
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s6 offset-s3">
           <input id="icon_prefix" type="text" class="validate">
           <label for="icon_prefix">State</label>
         </div>
       </div>
 
       <div class="row">
-        <div class="input-field col s6">
+        <div class="input-field col s6 offset-s3">
           <input id="icon_prefix" type="text" class="validate">
           <label for="icon_prefix">Zip</label>
         </div>
       </div>
 
+      <div class="row">
+  <button type = "submit">Submit</button>
+      </div>
 
     </form>
+    </div>
 
   </div>
 </template>
@@ -72,4 +78,15 @@
     font-size: 36px;
     text-align: center;
   }
+
+::-webkit-input-placeholder {
+ font-size: 14px;
+ color: #d0cdfa;
+ text-transform: uppercase;
+ text-transform: uppercase;
+ text-align: center;
+}
+
+input {text-align: center;}
+
 </style>
